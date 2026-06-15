0ff96cc8e0821571c9af1d904efa09322f970b86
Gayathiri18 <gayathrivenkatesan18@gmail.com>
2026-06-15 19:47:38 +0530

    Adding main dashboard and modified todo page-1

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-const CACHE = 'todo-v1';
+const CACHE = 'todo-v2';
 const FILES = ['/', '/index.html', '/style.css', '/app.js'];
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE).then(function (cache) {
       return cache.addAll(FILES);
+    })
+  );
+});
+
+// ADD THIS BLOCK: This forces the Service Worker to delete the old cache
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
